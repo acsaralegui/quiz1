21f93b1b97b6453f7fb0035da98441167da987ff
acsaralegui <aaron.saralegui@gmail.com>
2019-07-30 04:33:13 +0000

    creating the news form

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,4 +3,9 @@ class NewsController < ApplicationController
   def index
      @news = News.all
   end
+
+  def new
+    @news = News.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'techie#index'
+  resources :news
 end
